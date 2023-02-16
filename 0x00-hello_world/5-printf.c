bd535827c19bfbf03d798651dc4faa249c478654
Byzon12 <byzoneochieng@gmail.com>
2023-02-16 15:48:57 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - c prgram using printf function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("wth proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
