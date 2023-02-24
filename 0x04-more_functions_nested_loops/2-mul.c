70e79e0df3b5528b3cd44ca436a75b0985ddea28
Byzon12 <byzoneochieng@gmail.com>
2023-02-24 14:40:30 -0800

    multiplication of interges

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplication of two interges
+ * @a: first parameter
+ * @b: second parameter
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	int result = 0;
+
+	result = (a * b);
+	return (result);
+}
